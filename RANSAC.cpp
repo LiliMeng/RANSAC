d42d040bb90ac930c8572eb4ed06e628ec48df0f
LiliMeng <lilimeng1103@gmail.com>
2016-06-03 21:30:10 -0700

    Update RANSAC.cpp

--- RANSAC.cpp
+++ RANSAC.cpp
@@ -11,6 +11,10 @@
 #include <iomanip>
 #include <sstream>
 #include "RANSAC.h"
+#include "cvx_io.hpp"
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 using std::vector;
 
@@ -64,6 +68,29 @@ readData::readData(string filename)
 
     }
 
+}
+
+void visualize_world_coordinate()
+{
+    char rgb_img_file[] = "/home/lili/BMVC/7_scenes/chess/seq-03/frame-000594.color.png";
+    char depth_img_file[] = "/home/lili/BMVC/7_scenes/chess/seq-03/frame-000594.depth.png";
+    char pose_file[] = "/home/lili/BMVC/7_scenes/chess/seq-03/frame-000594.pose.txt";
+
+    cv::Mat camera_depth_img;
+    cv::Mat rgb_img;
+    bool is_read = cvx_io::imread_depth_16bit_to_64f(depth_img_file, camera_depth_img);
+    assert(is_read);
+    cvx_io::imread_rgb_8u(rgb_img_file, rgb_img);
+
+    cv::Mat pose = ms_7_scenes_util::read_pose_7_scenes(pose_file);
+    cv::Mat world_coordinate = ms_7_scenes_util::camera_depth_to_world_coordinate(camera_depth_img, pose);
+
+
+    vector<Mat> channels(3);
+    cv::split(world_coordinate, channels);
+    cvx_io::save_mat("85_point_cloud_x.txt", channels[0]);
+    cvx_io::save_mat("85_point_cloud_y.txt", channels[1]);
+    cvx_io::save_mat("85_point_cloud_z.txt", channels[2]);
 }
 
 readPose::readPose(){};
@@ -158,22 +185,52 @@ struct PreemptiveRANSACParameter
 public:
     PreemptiveRANSACParameter()
     {
-        reproj_threshold = 15.0;
+        reproj_threshold = 4.0;
     }
 };
 
+/*
+bool preemptiveRANSAC_lili(const vector<cv::Point3d> & wld_pts,
+                           const vector<cv::Point2d> & img_pts,
+                           const cv::Mat & camera_matrix,
+                           const cv::Mat & rvec,
+                           const cv::Mat & tvec,
+                           const cv::Mat & dist_coeff,
+                           const PreemptiveRANSACParameter & param)
+{
+    assert(img_pts.size() == wld_pts.size());
 
-bool preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
-                                         const vector<cv::Point3d> & wld_pts,
-                                         const cv::Mat & camera_matrix,
-                                         const cv::Mat & dist_coeff,
-                                         const PreemptiveRANSACParameter & param,
-                                         cv::Mat & camera_pose)
+    const int num_iteration = 2048;
+    int K = 1024;
+    const int N = (int)img_pts.size();
+    const int B = 500;
+
+
+     //sample random set
+    vector<cv::Point2d> four_img_pts;
+    vector<cv::Point3d> four_wld_pts;
+
+    ///randomly draw an hypothesis by sampling 4 pixels, first pixel randomly sample, the other three
+        int index = rand()%N;
+        four_img_pts.push_back(getData.img_pts[index]);
+        four_wld_pts.push_back(getData.pred_wld_pts[index]);
+    }
+
+
+}*/
+
+
+bool preemptiveRANSAC(const vector<cv::Point3d> & wld_pts,
+                      const vector<cv::Point2d> & img_pts,
+                      const cv::Mat & camera_matrix,
+                      const cv::Mat & dist_coeff,
+                      const PreemptiveRANSACParameter & param,
+                      cv::Mat & camera_pose)
 {
     assert(img_pts.size() == wld_pts.size());
     assert(img_pts.size() > 500);
 
-    const int num_iteration = 2048;
+    const int num_iteration = 1000000;
     int K = 1024;
     const int N = (int)img_pts.size();
     const int B = 500;
@@ -182,6 +239,7 @@ bool preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
 
     for(int i=0; i<num_iteration; i++)
     {
+        cout<<i<<endl;
         int k1 = 0;
         int k2 = 0;
         int k3 = 0;
@@ -216,11 +274,12 @@ bool preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
             rt_candidate.push_back(std::make_pair(rvec, tvec));
         }
 
+        /*
         if(rt_candidate.size()> K)
         {
-            printf("initialization repeat %d times\n", i);
+           // printf("initialization repeat %d times\n", i);
             break;
-        }
+        }*/
     }
 
     printf("init camera parameter number is %lu\n", rt_candidate.size());
@@ -251,11 +310,16 @@ bool preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
         }
 
         //count outliers
+        cout<<"losses.size() is "<<losses.size()<<endl;
+
         for(int i=0; i<losses.size(); i++)
         {
             //evaluate the accuracy by check reprojection error
             vector<cv::Point2d> projected_pts;
-            cv::projectPoints(sampled_img_pts, losses[i].rvec_, losses[i].tvec_, camera_matrix, dist_coeff, projected_pts);
+
+            cv::projectPoints(sampled_wld_pts, losses[i].rvec_, losses[i].tvec_, camera_matrix, dist_coeff, projected_pts);
+
+
             for(int j=0; j<projected_pts.size(); j++)
             {
                 cv::Point2d dif = projected_pts[j] - sampled_img_pts[j];
@@ -271,6 +335,7 @@ bool preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
                 }
             }
         }
+
 
         std::sort(losses.begin(), losses.end());
         losses.resize(losses.size()/2);
@@ -339,14 +404,18 @@ bool preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
 
 int main()
 {
+     ofstream fout("/home/lili/PatternRecognition/RANSAC/estimated_poses.txt");
      string rgb_loc="/home/lili/BMVC/7_scenes/chess/seq-03/frame-000594.color.png";
-     Mat rgb_frame=imread(rgb_loc, CV_LOAD_IMAGE_GRAYSCALE);
-     assert(rgb_frame.type()==CV_8U);
+     Mat rgb_frame=imread(rgb_loc,  CV_LOAD_IMAGE_COLOR);
+     assert(rgb_frame.type()==CV_8UC3);
+
+
      //imshow("RGB",rgb_frame);
 
      string depth_loc="/home/lili/BMVC/7_scenes/chess/seq-03/frame-000594.depth.png";
      Mat depth_frame=imread(depth_loc, CV_LOAD_IMAGE_ANYDEPTH);
-     assert(depth_frame.type()==CV_16U);
+     assert(depth_frame.type() == CV_16UC1);
+     depth_frame.convertTo(depth_frame, CV_64F);
      //imshow("Depth",depth_frame);
      //waitKey(0);
 
@@ -355,8 +424,8 @@ int main()
      readData getData(data_loc);
      readPose gtPose;
 
-
-     cout<<"ground truth pose is "<<gtPose.getPose(pose_loc)<<endl;
+     Mat gt_pose=gtPose.getPose(pose_loc);
+     cout<<"ground truth pose is "<<gt_pose<<endl;
 
 
      Mat rvec;
@@ -369,7 +438,6 @@ int main()
 
     cv::Mat cameraMatrix(3,3,CV_64F);
 
-
     cameraMatrix.at<double>(0,0)=fx;
     cameraMatrix.at<double>(0,1)=0;
     cameraMatrix.at<double>(0,2)=cx;
@@ -380,11 +448,47 @@ int main()
     cameraMatrix.at<double>(2,1)=0;
     cameraMatrix.at<double>(2,2)=1;
 
-    cv::solvePnPRansac(Mat(getData.pred_wld_pts), Mat(getData.img_pts), cameraMatrix, Mat(), rvec, tvec, false, 1000, 8.0);
+    //cv::solvePnPRansac(Mat(getData.pred_wld_pts), Mat(getData.img_pts), cameraMatrix, Mat(), rvec, tvec, false, 1000, 8.0);
+    //cv::solvePnP(Mat(getData.pred_wld_pts), Mat(getData.img_pts), cameraMatrix, Mat(), rvec, tvec, false, CV_EPNP);
+    int N=getData.gt_wld_pts.size();
+    cout<<"The number of points is "<<getData.gt_wld_pts.size()<<endl;
+
+
+    int iteration_num=10000;
+    vector<double> trans_error_vec;
+    vector<double> rot_error_vec;
+
+    for(int i=0; i<iteration_num; i++)
+    {
+
+     //sample random set
+    vector<cv::Point2d> four_img_pts;
+    vector<cv::Point3d> four_wld_pts;
+
+    for(int i=0; i<4; i++)
+    {
+        int index = rand()%N;
+        four_img_pts.push_back(getData.img_pts[index]);
+        four_wld_pts.push_back(getData.pred_wld_pts[index]);
+    }
+
+
+    cv::solvePnP(Mat(four_wld_pts), Mat(four_img_pts), cameraMatrix, Mat(), rvec, tvec, false, CV_P3P);
+
+
+    const PreemptiveRANSACParameter param;
+    cv::Mat camera_pose = cv::Mat::eye(4, 4, CV_64F);
+
+
+    //preemptiveRANSAC(Mat(getData.pred_wld_pts), Mat(getData.img_pts), cameraMatrix, Mat(), param, camera_pose);
+
+
+    //cv::solvePnPRansac(Mat(getData.pred_wld_pts), Mat(getData.img_pts), cameraMatrix, Mat(), rvec, tvec, false, 1000, 8.0);
     // change to camera to world transformation
+
+
     Mat rot;
     cv::Rodrigues(rvec, rot);
-    cv::Mat camera_pose = cv::Mat::eye(4, 4, CV_64F);
 
     for(int j=0; j<3; j++)
     {
@@ -398,20 +502,77 @@ int main()
     camera_pose.at<double>(1, 3) = tvec.at<double>(1, 0);
     camera_pose.at<double>(2, 3) = tvec.at<double>(2, 0);
 
+
     // camera to world coordinate
     camera_pose = camera_pose.inv();
 
 
-    cout<<"calculated camera_pose is "<<camera_pose<<endl;
+    //cout<<"calculated camera_pose is "<<camera_pose<<endl;
 
-    /*
-    preemptiveRANSAC(const vector<cv::Point2d> & img_pts,
-                                         const vector<cv::Point3d> & wld_pts,
-                                         const cv::Mat & camera_matrix,
-                                         const cv::Mat & dist_coeff,
-                                         const PreemptiveRANSACParameter & param,
-                                         cv::Mat & camera_pose)
-    */
+
+
+    ///rotation error between ground truth and the predicted data
+    Eigen::Matrix3d gt_rot;
+    gt_rot<< gt_pose.at<double>(0,0), gt_pose.at<double>(0,1), gt_pose.at<double>(0,2),
+             gt_pose.at<double>(1,0), gt_pose.at<double>(1,1), gt_pose.at<double>(1,2),
+             gt_pose.at<double>(2,0), gt_pose.at<double>(2,1), gt_pose.at<double>(2,2);
+
+
+    Eigen::Quaterniond gt_quater(gt_rot);
+
+
+   // Eigen::Quaterniond q1(rot);
+
+    Eigen::Matrix3d pred_rot;
+    pred_rot<<camera_pose.at<double>(0,0), camera_pose.at<double>(0,1), camera_pose.at<double>(0,2),
+              camera_pose.at<double>(1,0), camera_pose.at<double>(1,1), camera_pose.at<double>(1,2),
+              camera_pose.at<double>(2,0), camera_pose.at<double>(2,1), camera_pose.at<double>(2,2);
+
+    Eigen::Quaterniond pred_quater(pred_rot);
+
+    gt_quater.normalize();
+    pred_quater.normalize();
+
+    double val_dot = fabs(gt_quater.dot(pred_quater));
+    double error_rot = 2.0 *acos(val_dot)*180.0 / M_PI;
+    //printf("rotation error is %lf degrees\n", error_rot);
+
+
+    ///Translation error between ground truth and the predicted data
+    Eigen::Vector3d gt_trans(3);
+    gt_trans<<gt_pose.at<double>(0,3), gt_pose.at<double>(1,3), gt_pose.at<double>(2,3);
+
+    Eigen::Vector3d pred_trans(3);
+    pred_trans<<camera_pose.at<double>(0,3), camera_pose.at<double>(1,3), camera_pose.at<double>(2,3);
+
+    Eigen::Vector3d error_trans_3d=gt_trans-pred_trans;
+    double error_trans=error_trans_3d.norm();
+    //printf("translation error is %lf m\n", error_trans);
+
+    trans_error_vec.push_back(error_trans);
+    rot_error_vec.push_back(error_rot);
+    fout<<error_trans<<" "<<error_rot<<endl;
+
+
+    }
+
+    vector<double>::iterator min_trans_ite=std::min_element(std::begin(trans_error_vec),std::end(trans_error_vec));
+
+    int min_trans_position=std::distance(std::begin(trans_error_vec), min_trans_ite);
+
+    double min_trans_error=trans_error_vec[min_trans_position];
+    double rot_at_min_trans_error=rot_error_vec[min_trans_position];
+
+    cout<<"min_trans_error  "<<min_trans_error<<" m"<<" at position "<<min_trans_position<<" for this position, rot_error is "<<rot_at_min_trans_error<<" degrees"<<endl;
+
+
+    double min_rot_error=*std::min_element(std::begin(rot_error_vec),std::end(rot_error_vec));
+    vector<double>::iterator min_rot_ite=std::min_element(std::begin(rot_error_vec),std::end(rot_error_vec));
+    int min_rot_position=std::distance(std::begin(rot_error_vec), min_rot_ite);
+
+    double trans_at_min_rot_error=trans_error_vec[min_rot_position];
+
+    cout<<"min_rot_error  "<<min_rot_error<<" degrees."<<" at position "<<min_rot_position<<" for this position, trans_error is "<<trans_at_min_rot_error<<" m"<<endl;
 
     return 0;
 
