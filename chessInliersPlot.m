eb49edb9d523d7ab25c61d9ace7d210c6c2d90fc
LiliMeng <lilimeng1103@gmail.com>
2016-06-05 16:42:56 -0700

    Create chessInliersPlot.m

---
+++ chessInliersPlot.m
@@ -0,0 +1,54 @@
+clear all
+clc
+fileID1= fopen('/home/lili/PatternRecognition/RANSAC/chess_inliers_result/inliers_tran_rot_error_200images11.txt');
+C = textscan(fileID1, '%s %s %s %s');
+
+
+[N,d]=size(C{1});
+
+m2=str2mat(C{2});
+m3=str2mat(C{3});
+m4=str2mat(C{4});
+
+
+for i = 1:N
+    inliers(i,:)=str2double(m2(i,:));
+    trans_error(i,:)=str2double(m3(i,:));
+    rot_error(i,:)=str2double(m4(i,:));
+end
+
+figure 
+
+plot(inliers, trans_error, 'o');
+t=title('inliers number Vs translational error ');
+%le=legend('pchip data(16Hz)','pchip curve','raw data curve','raw data(18~20Hz)');
+%set(le, 'FontSize', 16);
+set(t, 'FontSize', 20);
+xl=xlabel('inliers number');
+yl=ylabel('translational error(m)');
+set(xl, 'FontSize', 18);
+set(yl, 'FontSize', 18);
+axis([0 1000 0 1]);
+figure 
+
+plot(inliers, rot_error, '*');
+t=title('inliers number Vs rotational error ');
+%le=legend('pchip data(16Hz)','pchip curve','raw data curve','raw data(18~20Hz)');
+%set(le, 'FontSize', 16);
+set(t, 'FontSize', 20);
+xl=xlabel('inliers number');
+yl=ylabel('rotational error(degree)');
+set(xl, 'FontSize', 18);
+set(yl, 'FontSize', 18);
+axis([0,1000,0,15])
+
+figure
+plot(trans_error, rot_error, 'b*');
+t=title('translational Vs rotational error ');
+%le=legend('pchip data(16Hz)','pchip curve','raw data curve','raw data(18~20Hz)');
+%set(le, 'FontSize', 16);
+set(t, 'FontSize', 20);
+xl=xlabel('translational error(m)');
+yl=ylabel('rotational error(degree)');
+set(xl, 'FontSize', 18);
+set(yl, 'FontSize', 18);
